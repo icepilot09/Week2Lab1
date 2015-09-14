af01c4fd2b70fb9d80b6753e8d7d3ed9a34a9f97
[Michael] <michael@lewiscommunications.com>
2015-09-14 16:03:44 -0500

    added dates

--- library.js
+++ library.js
@@ -3,7 +3,11 @@ var library = (function() {
 	TimeStamp: (function(){
    	  return {
 		UnixTimestamp: function(){},
-		UnixMillisecond: function(){}
+		UnixMillisecond: function(){
+			var date = new Date()
+			var milliSec = date.getMilliseconds();
+			return (milliSec);
+		}
 	  }
 	})(),
 	Local: (function(){
@@ -24,13 +28,21 @@ var library = (function() {
 	})(),
 	Second: (function(){
 		return{
-			Second: function(){},
+			Second: function(){
+				var date = new Date()
+				var dateSeconds = date.getSeconds();
+				return String(dateSeconds);
+			},
 			DblDigit: function(){}
 		}
 	})(),
 	Minute: (function(){
 		return{
-			Minute: function(){},
+			Minute: function(liveminute){
+				var date = new Date()
+				var dateMinute = date.getMinutes();
+				return String(dateMinute);
+			},
 			DblDigit: function(){}
 		}
 	})(),
@@ -48,9 +60,22 @@ var library = (function() {
 	})(),
 	Week: (function(){
 		return {
-			DayOfWeek: function(){},
-			AbrDayOfWeek: function(){},
-			FirstTwoOfWeek: function(){},
+			DayOfWeek: function(){
+				var date = new Date()
+				var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+  			return days[ date.getDay() ]
+
+			},
+			AbrDayOfWeek: function(){
+				var date = new Date()
+				var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+  			return days [date.getDay()].substr(0,3);
+			},
+			FirstTwoOfWeek: function(){
+				var date = new Date()
+				var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+  			return days [date.getDay()].substr(0,2);
+			},
 			WeekOfYear: function(){}
 		}
 	})(),
@@ -58,27 +83,99 @@ var library = (function() {
 		return {
 			DateOfMonth: (function(){
 				return {
-					Numeral: function(){},
-					Ordinal: function(){},
-					DateDblDigit: function(){}
+					Numeral: function(){
+						var date = new Date()
+						var dateNum = date.getDate();
+					return String(dateNum);
+					},
+					Ordinal: function(){
+					var date = new Date()
+					var dateOrd = date.getDate();
+						switch (date) {
+                			case 1:
+                			case 21:
+                			case 31:
+                    	return "" + dateOrd + "st";
+
+                			case 2:
+                			case 22:
+                    	return "" + dateOrd + "nd";
+
+                			case 3:
+                			case 23:
+                    	return "" + dateOrd + "rd";
+
+                		default:
+                    	return "" + dateOrd + "th";
+					}
+					
+					},
+					DateDblDigit: function(){
+					var date = new Date()
+					var dblDateDig = date.getDate();
+					if (dblDateDig < 10) {dblDateDig = '0'+ dblDateDig;}
+					return String (dblDateDig);
+					},
 				}
 			})(),
-			MonthNumber: function(){},
-			MonthNumberDblDigit: function(){},
-			AbrOfCurrentMonth: function(){},
-			CurrentMonth: function(){}
+			MonthNumber: function(){
+				var date = new Date()
+				var monthNum = date.getMonth() + 1;
+			return String(monthNum);
+			},
+			MonthNumberDblDigit: function(){
+				var date = new Date()
+				var monthNumDbl = date.getMonth() + 1;
+				if (monthNumDbl < 10) {monthNumDbl = '0'+ monthNumDbl;}
+			return String(monthNumDbl)
+			},
+			AbrOfCurrentMonth: function(){
+				var date = new Date();
+				var abrMonth = date.getMonth() + 1;
+				if (abrMonth < 10) {abrMonth = 'Sep';}
+			return String(abrMonth)
+			},
+			CurrentMonth: function(){
+				var date = new Date();
+				var currentMonth = date.getMonth() + 1;
+				if (currentMonth < 10) {currentMonth = 'September';}
+			return String(currentMonth);
+			}
 		}
 	})(),
 	Year: (function(){
 		return {
-			DayOfYear: (function(){
+			DayOfYear: (function(){	
 				return {
-					Numeral: function(){},
-					Ordinal: function(){}
-				}
+					Numeral: function(){
+						var now = new Date();
+                    	var start = new Date(now.getFullYear(), 0, 0);
+                    	var diff = now - start;
+                    	var oneDay = 1000 * 60 * 60 * 24;
+                    	var day = Math.floor(diff / oneDay);
+                    return String(day);
+                    },
+					Ordinal: function(){
+						var date = new Date();
+						var yearOrd = date.toDateString();
+						switch (date){
+							
+							
+						}
+						}
+					};
 			})(),
-			YearFull: function(){},
-			YearAbr: function(){}
+			YearFull: function(){
+				var date = new Date()
+				var yearFull = date.getFullYear();
+				return String(yearFull);
+			},
+			YearAbr: function(){
+				var date = new Date()
+				var yearFull = date.getFullYear();
+				return String(yearFull).substr(-2);
+				
+			}
 		}
 	})(),
 	Defaults: function(){}
